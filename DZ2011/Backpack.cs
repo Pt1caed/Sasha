733273409e8e7eff259cbc05446c1767e8114ff0
Pt1caed <pticaed000@gmail.com>
2024-11-20 22:16:09 +0200

    Add files via upload

---
+++ DZ2011/Backpack.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2011
+{
+    internal class Backpack
+    {
+        Item[] items;
+
+        public delegate void BackpackItem(Item item);
+        public event BackpackItem? OnBackpackAdd;
+        public event BackpackItem? OnBackpackRemove;
+
+        public Backpack(string color, string company, string typetextile, double weight, double volume, int size_backpack)
+        {
+            Color = color;
+            Company = company;
+            TypeTextile = typetextile;
+            Weight = weight;
+            Volume = volume;
+
+            items = new Item[size_backpack];
+            VolumeNow = 0;
+            
+        }
+
+        public double VolumeNow { get; private set; }
+        public string? Color { get; set; }
+        public string? Company { get; set; }
+        public string? TypeTextile { get; set; }
+        private double weight;
+        private double volume;
+
+        public void Add(Item item, int index)
+        {
+            if(index >= 0 && index < items.Length && VolumeNow + item.Volume < Volume)
+            {
+                items[index] = item;
+                OnBackpackAdd?.Invoke(item);
+                VolumeNow += item.Volume;
+            }
+        }
+        public void Remove(int index)
+        {
+            if (index >= 0 && index < items.Length)
+            {
+                OnBackpackRemove?.Invoke(items[index]);
+                VolumeNow -= items[index].Volume;
+
+                Item[] temp = new Item[items.Length - 1];
+
+
+                int counter = 0;
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if(i == index)
+                    {
+                        continue;
+                    }
+                    temp[counter] = items[i];
+                    counter++;
+                }
+                items = temp;
+            }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Weight can't <=0");
+                }
+                weight = value;
+            }
+        }
+        public double Volume
+        {
+            get { return volume; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Volume can't <=0");
+                }
+                volume = value;
+            }
+        }
+    }
+
+    struct Item
+    {
+        public Item(string name, double weight, double volume)
+        {
+            Name = name;
+            Weight = weight;
+            Volume = volume;
+        }
+        private double volume;
+
+        public string? Name { get; set; }
+        public double Weight { get; private set; }
+        public double Volume
+        {
+            get => volume;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Volume can't <=0");
+                }
+                volume = value;
+            }
+        }
+    }
+}
+
---
+++ DZ2011/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace DZ2011
+{
+    internal class Program
+    {
+        //2
+
+
+        static void Main(string[] args)
+        {
+            // 1
+            string? message = "Hello privet kakdela";
+
+            var SearchWord = (string message, string word) =>
+            {
+                message = message.ToLower();
+                if (message.Contains(word))
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            Console.WriteLine(SearchWord(message, "Hello"));
+            // 2
+            Backpack backpack = new("green", "Good", "Great", 10000, 1000, 10);
+
+            backpack.OnBackpackAdd += item =>
+            {
+                Console.WriteLine($"Предмет {item.Name} был положен в рюкзак. +{item.Volume}к Volume");
+            };
+            backpack.OnBackpackRemove += item =>
+            {
+                Console.WriteLine($"Предмет {item.Name} был убран из рюкзака. -{item.Volume} к Volume");
+            };
+
+            Item item1 = new("Har", 10, 10);
+            backpack.Add(item1, 1);
+
+            // 3
+
+            int[] massive = [-1, -2, -3, 1, 4, 5, 6];
+
+            var sort_positive = (int[] massive) =>
+            {
+                int count = 0;
+
+                for (int i = 0; i < massive.Length; i++)
+                {
+                    if (massive[i] > 0) { count++; }
+                }
+                return count;
+            };
+
+            Console.WriteLine(sort_positive(massive));
+        }
+    }
+}
