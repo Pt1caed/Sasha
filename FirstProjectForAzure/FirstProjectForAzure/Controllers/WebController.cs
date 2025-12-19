e2c549653ce574a316a19b8dd6918394f71ceb05
Pt1caed <pticaed000@gmail.com>
2025-12-19 19:29:51 +0200

    Add files via upload

---
+++ FirstProjectForAzure/FirstProjectForAzure/Controllers/WebController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProjectForAzure.Controllers
+{
+    [Route("api/web/")]
+    [ApiController]
+    public class WebController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public IActionResult GetHello()
+        {
+            return Ok("Hello, World!");
+        }
+        [HttpGet("time")]
+        public IActionResult Time()
+        {
+            return Ok(DateTime.Now.ToString());
+        }
+        [HttpPost("sum")]
+        public IActionResult Sum(int a, int b)
+        {
+            return Ok(a + b);
+        }
+    }
+}
